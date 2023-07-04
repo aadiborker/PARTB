9b21c667d5e4a0f0c2d6511f5a344f6a6bea43cf
aadiborker <121426563+aadiborker@users.noreply.github.com>
2023-07-04 18:19:16 +0530

    Update 3.js

--- 3.js
+++ 3.js
@@ -2,7 +2,6 @@ function performOperation(a, b, operator) {
     return new Promise((resolve, reject) => {
       setTimeout(() => {
         let result;
-  
         switch (operator) {
           case '+':
             result = a + b;
@@ -19,18 +18,15 @@ function performOperation(a, b, operator) {
           default:
             reject("Invalid operator");
         }
-  
         resolve(result);
       }, 1000); // Delay of 1 second
     });
   }
-  
   async function performOperations() {
     try {
       const operand1 = Number(prompt("Enter the first operand:"));
       const operator = prompt("Enter the operator (+, -, *, /):");
       const operand2 = Number(prompt("Enter the second operand:"));
-  
       const result = await performOperation(operand1, operand2, operator);
       alert("Result:"+ result);
     } catch (error) {
