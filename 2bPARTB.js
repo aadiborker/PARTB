c54a18a4a8e707ceee13cc3672e6cd8367872367
aadiborker <121426563+aadiborker@users.noreply.github.com>
2023-07-04 17:50:28 +0530

    Create 2bPARTB.js

---
+++ 2bPARTB.js
@@ -0,0 +1,8 @@
+// Prompt the user to enter a number
+var number = parseInt(prompt("Enter a number:"));
+// Check if the number is divisible by 2
+if (number % 2 === 0) {
+  alert(number + " is an even number.");
+} else {
+  alert(number + " is an odd number.");
+}
